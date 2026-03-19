543dfac458046b05dbd1dfaf0ae4ecb4cbfe55bf
maxsimilian560 <maxsimilian560@gmail.com>
2026-03-20 01:07:47 +0300

    first commit

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <vector>
 #include <cstring>
+#include <bit>
 struct ListNode { // ListNode модифицировать нельзя
   ListNode* prev = nullptr; // указатель на предыдущий элемент или nullptr
   ListNode* next = nullptr;
@@ -9,11 +10,10 @@ struct ListNode { // ListNode модифицировать нельзя
   std::string data; // произвольные пользовательские данные 
 };
 //Я бы добавил переменную rand_index, или заменил бы rand на неё, но раз модифицировать нельзя...
-
 class List {
   ListNode* head = nullptr;
   ListNode* last = nullptr;
-  size_t count = 0;
+  uint64_t count = 0;
 public:
   List() {
     //Могу использовать new если необходимо, просто я обычно работаю с malloc/calloc/realloc/free
@@ -36,42 +36,42 @@ public:
     last = last->next;
     count++;
   }
-  size_t size() {
+  uint64_t size() {
     return count;
   }
-  ListNode* get(long long index) {
+  ListNode* get(int64_t index) {
     if (index < 0) return nullptr;
     if (index >= count) throw std::out_of_range("Index out of range '" + std::to_string(index) + "'/'" + std::to_string(count) + "'");
     ListNode* curr = head;
-    for (size_t i = 0; i < index; i++) {
+    for (uint64_t i = 0; i < index; i++) {
       curr = curr->next;
     }
     return curr;
   }
-  long long get_index(ListNode* in) {
+  int64_t get_index(ListNode* in) {
     ListNode* curr = head;
     if (in == nullptr) return -1;
-    for (size_t i = 0; i < count; i++) {
+    for (uint64_t i = 0; i < count; i++) {
       if (curr == in) return i;
       curr = curr->next;
     }
-    throw std::out_of_range("Node '" + std::to_string((long long)in) + "' not found");
+    throw std::out_of_range("Node '" + std::to_string((int64_t)in) + "' not found");
     //Обычно я не так часто использую throw, а использую свою систему логирования из своего фреймворка.
     //Но думаю для тестового задания это будет лишним
   }
   void print() {
     ListNode* curr = head;
-    size_t index = 0;
-    size_t temp;
+    uint64_t index = 0;
+    uint64_t temp;
     while (curr->next != nullptr) {
       std::cout << "- Узел " << index++ << ": \"" << curr->data << "\"" << " -> rand на узел " << ((temp = get_index(curr->rand)) == -1 ? "nullptr" : std::to_string(temp)) << std::endl;
       curr = curr->next;
     }
   }
-  void remove(size_t index) {
+  void remove(uint64_t index) {
     if (index >= count) throw std::out_of_range("Index out of range");
     ListNode* curr = head;
-    for (size_t i = 0; i < index; i++) {
+    for (uint64_t i = 0; i < index; i++) {
       curr = curr->next;
     }
     curr->prev->next = curr->next;
@@ -80,7 +80,7 @@ public:
     free(curr);
     count--;
   }
-  ~List() {
+  void clear() {
     if (head == nullptr) return;
     ListNode* curr = head;
     while (curr != nullptr) {
@@ -90,48 +90,154 @@ public:
       curr = next;
     }
   }
+  ~List() {
+    clear();
+  }
+  void save_to_file(std::string path) {
+    std::ofstream file(path, std::ios::binary);
+    file.write("taskmagic\n", 10);
+    file.write(reinterpret_cast<const char*>(&count), sizeof(count));
+    ListNode* curr = head;
+    for (uint64_t i = 0; i < count; ++i) {
+      uint64_t len = curr->data.size();
+      int64_t index_rand = get_index(curr->rand);
+      if constexpr (std::endian::native == std::endian::big) {
+        char* _len = reinterpret_cast<char*>(&len);
+        char* _index_rand = reinterpret_cast<char*>(&index_rand);
+        std::reverse(_len, _len + sizeof(uint64_t));
+        std::reverse(_index_rand, _index_rand + sizeof(int64_t));
+      }
+
+      file.write(reinterpret_cast<const char*>(&len), sizeof(len));
+      file.write(curr->data.c_str(), len);
+      file.write(reinterpret_cast<const char*>(&index_rand), sizeof(index_rand));
+      curr = curr->next;
+    }
+    std::cout<<"Save to file: "<<path<<std::endl;
+    file.close();
+  }
+  int load_from_file(std::string path) {
+    std::ifstream file(path, std::ios::binary);
+    if (!file.is_open()) return 1;
+    char magic[10];
+    file.read(magic, 10);
+    if (!file || memcmp(magic, "taskmagic\n", 10) != 0)
+      return 1;
+    uint64_t file_count;
+    file.read(reinterpret_cast<char*>(&file_count), sizeof(file_count));
+    if (!file) throw std::runtime_error("Read error");
+    clear();
+    head = (ListNode*)calloc(1, sizeof(ListNode));
+    last = head;
+    count = 0;
+    std::vector<int64_t> temp_rand;
+    temp_rand.reserve(file_count);
+    for (uint64_t i = 0; i < file_count; i++) {
+      uint64_t len;
+      file.read(reinterpret_cast<char*>(&len), sizeof(len));
+      if constexpr (std::endian::native == std::endian::big) {
+        char* _len = reinterpret_cast<char*>(&len);
+        std::reverse(_len, _len + sizeof(uint64_t));
+      }
+      std::string data(len, '\0');
+      file.read(&data[0], len);
+
+      int64_t index_rand;
+      file.read(reinterpret_cast<char*>(&index_rand), sizeof(index_rand));
+      if constexpr (std::endian::native == std::endian::big) {
+        char* _index_rand = reinterpret_cast<char*>(&index_rand);
+        std::reverse(_index_rand, _index_rand + sizeof(int64_t));
+      }
+      temp_rand.push_back(index_rand);
+      this->push(data, nullptr);
+    }
+    ListNode* curr = head;
+    for (uint64_t i = 0; i < file_count; i++) {
+      const int64_t& lnk = temp_rand[i];
+      curr->rand = (lnk == -1) ? nullptr : get(lnk);
+      curr = curr->next;
+    }
+    if (count > 0) {
+      last->prev = get(count - 1);
+    }
+    else {
+      last->prev = nullptr;
+    }
+    std::cout<<"Load from file: "<<path<<std::endl;
+    return 0;
+  }
 };
 int main(int argc, char* argv[]) {
   std::string infile = "inlet.in";
   if (argc > 1) {
+    if (std::string(argv[1]) == "-h" || std::string(argv[1]) == "--help") {
+      std::cout << \
+        R"(Usage: ./test [OPTIONS] [infile] [outfile]
+
+Description:
+  Parses a text file into a custom linked list structure.
+  Supports serialization/deserialization to binary format.
+
+Arguments:
+  infile   Path to input text file (default: inlet.in)
+  outfile  Path to output binary file (default: outlet.out)
+
+Options:
+  -h, --help    Show this help message and exit
+
+Examples:
+  ./test                      # Uses default inlet.in
+  ./test data.txt             # Read from data.txt
+  ./test data.bin save.bin    # Read + save to binary
+)" << std::endl;
+      return 0;
+    }
     infile = argv[1];
   }
-  std::ifstream in(infile, std::ios::ate);
-  if (!in.is_open()) { std::cerr << "Error opening file '" << infile << "'" << std::endl;return 1; }
-  size_t size = in.tellg();
-  if (size == -1) {
-    std::cerr << "File is empty" << std::endl;
-    return 1;
+  std::string outfile = "outlet.out";
+  if (argc > 2) {
+    outfile = argv[2];
   }
-  in.seekg(0, std::ios::beg);
-  std::string text;
-  text.resize(size);
-  in.read(text.data(), size);
   List list;
-  ListNode temp;
-  size_t start = 0;
-  size_t end = text.find(';', start);
-  while (end != std::string::npos) {
-    size_t data_len = end - start;
-    temp.data.assign(text.data() + start, data_len);
-    start = end + 1;
-    end = text.find('\n', start);
-    if (end == std::string::npos) {
-      end = text.size();
+  if (list.load_from_file(infile)) {
+
+    std::ifstream in(infile, std::ios::ate);
+    if (!in.is_open()) { std::cerr << "Error opening file '" << infile << "'" << std::endl;return 1; }
+    uint64_t size = in.tellg();
+    if (size == -1) {
+      std::cerr << "File is empty" << std::endl;
+      return 1;
     }
-    if (end <= start)continue;
-    temp.rand = reinterpret_cast<ListNode*>(std::stoi(text.substr(start, end - start)));
-    list.push(temp);
-    start = end + 1;
-    end = text.find(';', start);
-    if (end == std::string::npos) {
-      break;
+    in.seekg(0, std::ios::beg);
+    std::string text;
+    text.resize(size);
+    in.read(text.data(), size);
+    ListNode temp;
+    uint64_t start = 0;
+    uint64_t end = text.find(';', start);
+    while (end != std::string::npos) {
+      uint64_t data_len = end - start;
+      temp.data.assign(text.data() + start, data_len);
+      start = end + 1;
+      end = text.find('\n', start);
+      if (end == std::string::npos) {
+        end = text.size();
+      }
+      if (end <= start)continue;
+      temp.rand = reinterpret_cast<ListNode*>(std::stoi(text.substr(start, end - start)));
+      list.push(temp);
+      start = end + 1;
+      end = text.find(';', start);
+      if (end == std::string::npos) {
+        break;
+      }
     }
-  }
-  //Для того, чтоб изменить индексы на указатели в rand
-  for (size_t i = 0; i < list.size(); i++) {
-    list.get(i)->rand = list.get((long long)list.get(i)->rand);
+    //Для того, чтоб изменить индексы на указатели в rand
+    for (uint64_t i = 0; i < list.size(); i++) {
+      list.get(i)->rand = list.get((int64_t)list.get(i)->rand);
+    }
   }
   list.print();
+  list.save_to_file(outfile);
   return 0;
 }
